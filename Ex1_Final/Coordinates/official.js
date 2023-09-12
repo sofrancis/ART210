c2daa37caddda100573189183227644974ac6b58
sofrancis <sofrancis03@gmail.com>
2023-09-12 12:41:58 -0700

    Ex1_Final

---
+++ Ex1_Final/Coordinates/official.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(215, 213, 201);
+  strokeWeight(2);
+  stroke(72);
+  quad(68, 8.55, 22, 69.55, 68, 129.55, 111, 69.55); 
+  fill(252, 129, 1);
+  
+  ellipse(139, 27.55, 5);
+}
