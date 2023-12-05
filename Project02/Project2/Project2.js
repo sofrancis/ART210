cb9270c5bad7d5294e11030b1d977bcf78be40c8
sofrancis <sofrancis03@gmail.com>
2023-12-05 15:29:52 -0800

    slayyarific

--- Project02/Project2/Project2.js
+++ Project02/Project2/Project2.js
@@ -1,25 +1,34 @@
 //arrays for images shown, goodies, and baddies
 let infidelImg = [];
+//arrays for all the sins of individual infidels
 let goodie = ['- Decapitation\n- Infidelity', '- Filthy conversation\n- Fratercide', '- Not correcting children\n- Excessively lustful', '- Unnatural beauty\n- Cannibalism', '- Killed a parent \n- Returned an insult', '- Wrathful\n- Rudeness', '- Judges others unfairly\n- Youthful lust', '- Overindulging\n- Enslaved to their passions', '- Stubborness\n- Refusing to die for others', '- Seeking riches\n- Sorceries'];
 let baddie = ['- Unclean\n- Accepting bribes', '- Sloth\n- Urging others to violence', '- Complaining about nothing\n- Devious', '- Threatening children\n- Busybody', '- Cursing innocents\n- Became a cult leader', '- Tempting others\n- Blinding a child', '- Falsely convicted another\n- Covetous', '- Not submitting to a law\n- Greedy', '- Envious\n- Vain'];
-let allInfidels = shuffle(goodie.concat(baddie));
-let currentInfidel;
 
-let score = 0;
-let decisionsMade = 0;
-let progressBarWidth;
+let allInfidels = shuffle(goodie.concat(baddie)); //shufles goodies and baddies together
+let currentInfidel; //defines the current infidel as a variable
 
-let occuText = [];
+let score = 0; //sets score to zero
+let decisionsMade = 0; //sets the amount of decisions made to zero
+
+let progressBarDiameter; //sets the progress bar as a variable
+
+//images for winning, losing, and the white figure displayed while playing
 let winner;
 let loser;
-
 let blessed;
+
 //buttons to reject or bless
 let blessButton;
 let rejectButton;
-//fancy circles
-let choices;
-let rating;
+
+let choices; //image that contains the bless and reject buttons
+let rating; //image that contains the progress bar
+
+//button to restart game
+let restartButton;
+
+//variable to hide progress bar once the game is over
+let gameOver = false;
 
 function preload(){
 //preload background image and fancy circles
@@ -28,6 +37,7 @@ function preload(){
   rating = loadImage('data/fillin.png');
   winner = loadImage('data/superblessed.png');
   loser = loadImage('data/otherblessed.png');
+  superloser = loadImage('data/notblessed.png');
 
 //array of infidel images
   infidelImg[0] = loadImage('data/infidels/infidelfr.png');
@@ -35,96 +45,133 @@ function preload(){
   infidelImg[2] = loadImage('data/infidels/blueinfidel.png');
   infidelImg[3] = loadImage('data/infidels/greeninfidel.png');
 
-  occuText[0] = "Officer";  
-  randomInfidelIndex = floor(random(infidelImg.length));
+  randomInfidelIndex = floor(random(infidelImg.length)); //displays a random infidel image
 }
 
 function setup() {
-  createCanvas(1200, 1024);
+  createCanvas(1200, 1024); //creates canvas
   
-  blessButton = createButton('Bless');
-  blessButton.size(150,80);
+  blessButton = createButton('Bless'); //creates button that reads "Bless"
+  blessButton.size(150,80); //button size
   blessButton.position(36, 558);
-  blessButton.style('background-color', '#00000000');
-  blessButton.style('border', '#000000');
-  blessButton.style('font-family', 'Fondamento');
-  blessButton.style('font-size', '30px');  
-  blessButton.style('color', '#DDA75E');
-  blessButton.mousePressed(blessInfidels);
+  blessButton.style('background-color', '#00000000'); //sets background of the button to transparent
+  blessButton.style('border', '#000000'); //sets border of the button to transparent
+  blessButton.style('font-family', 'Fondamento'); //sets font to Fondamento
+  blessButton.style('font-size', '40px'); //font size
+  blessButton.style('color', '#DDA75E'); // tan
+  blessButton.show(); //shows button
+  blessButton.mousePressed(blessInfidels); //bleses an infidel when pressed
   
-  rejectButton = createButton('Reject');
-  rejectButton.size(145, 80);
-  rejectButton.position(38, 741);
-  rejectButton.style('background-color', '#00000000');
-  rejectButton.style('border', '#000000');
-  rejectButton.style('font-family', 'Fondamento');
-  rejectButton.style('font-size', '28px');
-  rejectButton.style('color', '#B26D35');
-  rejectButton.mousePressed(rejectInfidels);  
+  rejectButton = createButton('Reject'); //creates button that reads "Reject"
+  rejectButton.size(145, 80); //button size
+  rejectButton.position(38, 765);
+  rejectButton.style('background-color', '#00000000'); //sets background of the button to transparent
+  rejectButton.style('border', '#000000'); //sets border of the button to transparent
+  rejectButton.style('font-family', 'Fondamento'); //sets font to Fondamento
+  rejectButton.style('font-size', '35px'); //font size
+  rejectButton.style('color', '#DDA75E'); //tan
+  rejectButton.show(); //shows button
+  rejectButton.mousePressed(rejectInfidels); //rejects an infidel when pressed
   
-  progressBarWidth = width / 2;
+  restartButton = createButton('Continue\nPenance?'); //creates button that reads "Continue Penance?"
+  restartButton.size(145, 80); //button size
+  restartButton.position(900, 400);
+  restartButton.style('background-color', '#00000000'); //sets background of the button to transparent
+  restartButton.style('border', '#000000'); //sets border of the button to transparent
+  restartButton.style('font-family', 'Fondamento'); //sets font to Fondamento
+  restartButton.style('font-size', '60px'); //font size
+  restartButton.style('color', '#FFFFFF'); //white
+  restartButton.hide(); //hides button
+  restartButton.mousePressed(resetButtonPressed); //resets game when pressed
+  
+  progressBarDiameter = 200; //sets max size of the progress bar to 200 pixels
   currentInfidel = allInfidels.pop();
+}
+
+function reset(){
+  blessButton.show(); //displays bless button
+  rejectButton.show(); //displays reject button
+  restartButton.hide(); //hides the reset button
   
+  score = 0; //resets score back to zero
+  decisionsMade = 0; //resets decisions back to zero
+  gameOver = false;
+  progressBarDiameter = 200; //resets progress bar to "zero"
+  currentInfidel = allInfidels.pop(); //resets infidels
+  
+  loop(); //begins draw function
+}
+
+function resetButtonPressed() {
+  reset(); //executes the reset function above when the reset button is clicked on
 }
 
 function draw() {
-  background(255);
+  background(255); //sets the color of the background to black
   
-  image(blessed, 0, 0);
-  image(infidelImg[randomInfidelIndex], 400, 475, 500, 550);
-  image(choices, 30, 520);
-  image(choices, 30, 700);
-  choices.resize(160, 160);
+  image(blessed, 0, 0); //displays the main shape cloaked in white
+  image(infidelImg[randomInfidelIndex], 400, 475, 500, 550); //displays an image of an infidel
+  image(choices, 30, 520); //displays the top circle containing the bless button
+  image(choices, 30, 725); //displays the bottom circle containing the reject button
+  choices.resize(160, 160); //resizes the circles containing the bless/reject buttons
   
-  image(rating, 850, 250);
-  rating.resize(300, 300);
+  image(rating, 850, 250); //displays the circle containing the progress bar
+  rating.resize(300, 300); //resizes the circle containing the progress bar
   
   //top text
-  fill(255);
-  textSize(85);
-  text('Pr  tect th   In  oce  ts', 220, 135);
+  fill(255); //white
+  textSize(85); //text size
+  text('Pr  tect th   In  oce  ts', 220, 135); //displayed text
 
 //lil mystery message
-  textSize(86);
-  fill(225, 0, 0);
-  text('o', 305.1, 135.4);
-  text('e', 578, 135.4);
-  text('n', 712, 135.4);
-  text('n', 877, 135.4);  
+  textSize(86); //text size
+  fill(225, 0, 0); //red
+  text('o', 305.1, 135.4); //displayed letter
+  text('e', 578, 135.4); //displayed letter
+  text('n', 712, 135.4); //displayed letter
+  text('n', 877, 135.4); //displayed letter
   
-  fill(255); 
-  textSize(60);
-  text('Sins: \n\n\n\n\nSentencing:', 30, 300);
+  //text to explain future controls
+  fill(178, 109, 53); //dark tan
+  textSize(23); //text size
+  text('Raise Hand', 50, 705); //displays the text "Raise Hand"
+  text('Lower Hand', 50, 908); //displays the text "Lower Hand"
+  text(`Decisions: ${decisionsMade}/10`, 918, 580); //displays the amount of decisions left
   
-  fill(115, 124, 80); 
-  textSize(35);
-  textLeading(40);
-  text(`${currentInfidel}`, 50, 350);
-  text(`Decisions: ${decisionsMade}/10`, 880, 600);
+  //displays text for sins (color, size, spacing)
+  fill(115, 124, 80); //mild green
+  textSize(35); //text size
+  textLeading(40); //vertical text spacing
+  text(`${currentInfidel}`, 50, 350); //displays the current infidel's sins
   
-  textFont('Fondamento');
+  //white text for sins and sentencing
+  fill(255); //white
+  textSize(60); //text size
+  text('Sins: \n\n\n\n\nSentencing:', 30, 300); //displayed text  
+  textFont('Fondamento'); //sets font for all text
   
-  checkGameStatus();
-  displayProgressBar();
+  checkGameStatus(); //check game status 
+  displayProgressBar(); //self-explanatory
 }
 
 function displayProgressBar() {
-  let progress = map(score, -4, 4, 0, progressBarWidth); 
-  fill(100);
-  rect(width / 4, height / 2 - 10, progressBarWidth, 20); 
-  fill(0, 255, 0);
-  rect(width / 4, height / 2 - 10, progress, 20);
+  if (!gameOver) { //displays progress bar conditionally so that it disappears when the game is over
+  let progress = map(score, -4, 4, 0, progressBarDiameter, progressBarDiameter); //sets the progress location and the max size of the bar
+  fill(0, 255, 0); //color of progress bar
+  ellipse(1000, 400, progress, progress); //location and size of the progress bar
+ }
 }
 
 function keyPressed() {
   if (decisionsMade < 10 && currentInfidel) {
-    let decision = key === 'F'; 
-    updateScore(decision, currentInfidel);
-    currentInfidel = allInfidels.pop();
+    let decision = key === 'F'; //sets 'F' as the action key
+    updateScore(decision, currentInfidel); //lose one decision
+    currentInfidel = allInfidels.pop(); //pop infidels once decision is taken
     decisionsMade++;
   }
 }
 
+//FIX THIS
 function updateScore(decision, character) {
   let correctDecision = (goodie.includes(character) && decision) || (baddie.includes(character) && !decision);
   score += correctDecision ? 1 : -1;
@@ -133,21 +180,87 @@ function updateScore(decision, character) {
  
 function checkGameStatus() {
   if (score >= 4) {
-//    text('You Win!', width / 2, 300);
-    image(winner, 0, 0);
-    noLoop(); // Stops the draw loop
+    image(winner, 0, 0); //displays "winning" image
+    
+    //sentencing for ending
+    fill(255, 0, 0);
+    textSize(140);
+    text('Canonization', 200, 140);
+    
+    //explains sentencing
+    fill(221, 167, 94); //tan
+    textSize(40); //text size
+    text('Your good deeds\nwill not be\nremembered.', 50, 450);
+    
+    //text to instruct player
+    fill(169, 112, 64); //dark tan
+    textSize(23); //text size
+    text('Click on "Penance"', 930, 590); //displays text "Click on 'Penance'"
+    
+    blessButton.hide(); //hide bless button
+    rejectButton.hide(); //hide reject button
+    restartButton.show(); //show restart button
+    
+    gameOver = true; //hides progress bar
+    
+    noLoop(); //stops drawing function
+    
   } else if (score <= -4) {
-//    text('You Lose!', width / 2, 300);
-    noLoop(); // Stops the draw loop
+    image(superloser, 0, 0); // displays "losing" image
+    
+    //sentencing for ending
+    fill(255, 0, 0);
+    textSize(140);
+    text('Execution', 300, 140);    
+    
+    //explains sentencing
+    fill(221, 167, 94); //tan
+    textSize(40); //text size
+    text('You will live on\nin infamy.', 100, 500);
+    
+    //text to instruct player
+    fill(169, 112, 64); //dark tan
+    textSize(23); //text size
+    text('Click on "Penance"', 930, 590); //displays text "Click on 'Penance'"
+    
+    blessButton.hide();  //hide bless button
+    rejectButton.hide(); //hide reject button
+    restartButton.show(); //show restart button
+    
+    gameOver = true; //hides progress bar
+    
+    noLoop(); //stops drawing function
+    
   } else if (decisionsMade >= 10) {
-//    text('Game Over!', width / 2, 300);
-    image(loser, 0, 0);
-    noLoop(); // Stops the draw loop
+    image(loser, 0, 0); // displays "losing" image
+    
+    //sentencing for ending
+    fill(255, 0, 0);
+    textSize(140);
+    text('Excommunication', 70, 180);
+   
+    //explains sentencing
+    fill(221, 167, 94); //tan
+    textSize(40); //text size
+    text('You are weak.', 100, 620);
+    
+    //text to instruct player
+    fill(169, 112, 64); //dark tan
+    textSize(23); //text size
+    text('Click on "Penance"', 930, 590); //displays text "Click on 'Penance'"
+    
+    blessButton.hide();  //hide bless button
+    rejectButton.hide(); //hide reject button
+    restartButton.show(); //show restart button
+    
+    gameOver = true; //hides progress bar
+    
+    noLoop(); //stops drawing function
   }
 }
 
+//shuffles array of infidels
 function shuffle(array) {
-
   for (let i = array.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
@@ -156,9 +269,16 @@ function shuffle(array) {
 }
 
 function blessInfidels() {
-  
 }
 
 function rejectInfidels() {
-
 }
+
+//shit to do:
+//CHECK  - get button for the end to restart the game (find code and put in if/else)
+//check  - make buttons disappear at the end of the game
+//CHECK - fix progress bar (display cirle, increase/decrease in size)
+//CHECK  - make one lose after their score reaches 0
+//  - fix bless/reject to do either action when button is clicked/when hand is raised in the future
+// - shuffle images of infidels for each individual infidel up for trial
+//game glitches when 8 choices are taken when the game is reset
